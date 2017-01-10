d5fc5e8aa76ed480065b1e8498b4faf582dc305f
Lilou ROGER <lilouroger82@hotmail.fr>
2017-01-10 10:44:24 +0100

    Fix preview commit (dir error)

---
+++ db/seeds.rb
@@ -0,0 +1,16 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+User.create(email: "root@root.fr", password: "rootroot", password_confirmation: "rootroot")
+Post.create(content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit,")
+Post.create(content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit,")
+Post.create(content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit,")
+Post.create(content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit,")
+Post.create(content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit,")
+Post.create(content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit,")
+Post.create(content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit,")
+Post.create(content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit,")
